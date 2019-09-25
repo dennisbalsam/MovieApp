2750777a8440039c1aa793f817fd2affe11bef6d
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-09-25 16:53:20 -0400

    Modification

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.query('movies', {
-      t: 'Lion King'
+      s: 'Lion King'
     });
   }
   
