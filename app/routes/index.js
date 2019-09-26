ff1890b55c6915235df81a987dde69b092e25c44
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-09-26 16:19:26 -0400

    Changed Design and added user input

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,30 @@
 import Route from '@ember/routing/route';
+import movies from '../models/movies';
 
 export default Route.extend({
+  movieName: 'Lion King',
+  toggleList:function() {
+    document.getElementById("appear").style.display = "block";
+  },
+  newRecord:function()
+  {
+     //store the data from movie searched
+     this.store.query('movies', {
+      s: this.movieName
+    });
+  },
+  actions:{
+
+    getInput:function(name) {
+      //set the movie name to be input
+       this.movieName = name;
+      //display the movies
+       this.newRecord();
+    }
+  },
   model() {
     return this.store.query('movies', {
-      s: 'Avengers'
+      s: this.movieName
     });
   }
   
