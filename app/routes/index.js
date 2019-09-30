a980b7a95a7e2ee236f8f44d32f2e329d3f27ca8
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-09-30 13:03:18 -0400

    Added loading animation when searching for movie

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,18 +5,20 @@ export default Route.extend({
   movieName: 'Avengers',
   displayselected: false,
   toggleList:function() {
+    document.getElementById("loader").style.display = "block";
     document.getElementById("appear").style.animation = "3s fadeIn forwards";
-    document.getElementById("appear").style.animationDelay = "1s";
+    document.getElementById("appear").style.animationDelay = "5.1s";
     document.getElementById("search-submit").style.display = "none";
+    setTimeout(function(){ document.getElementById("loader").style.display = "none"; }, 5000);
   },
   newRecord:function()
   {
-    
    this.refresh(); 
   },
   actions:{
 
     getInput:function(name) {
+        
       //set the movie name to be input
        this.movieName = name;
       //display the movies
